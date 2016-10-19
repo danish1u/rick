916a9d2d0872e9ca5194709de62cd9b981e5d307
danish1u <danishprince786a@gmail.com>
2016-10-20 00:27:18 +0500

    Create sliderconfig.js

---
+++ js/sliderconfig.js
@@ -0,0 +1,20 @@
+        jQuery(document).ready(function($) {
+          $('#banner-fade').bjqs({
+            height      : 450,
+            width       : 980,
+            responsive  : true
+          });
+        });
+      </script><pre class="secret-source"><code>jQuery(document).ready(function($) {
+  $('#banner-fade').bjqs({
+    height      : 450,
+    width       : 980,
+    responsive  : true
+  });
+});</code></pre>
+<script>
+    jQuery(function($) {
+        $('.secret-source').secretSource({
+            includeTag: false
+        });
+    });

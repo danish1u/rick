55b32894c15ffcc64ad91abb1ac3ae2d473fd9c8
danish1u <danishprince786a@gmail.com>
2016-11-01 01:11:21 +0500

    Create good.js

---
+++ template/good.js
@@ -0,0 +1,19 @@
+jQuery(document).ready(function($) {
+        $('#banner-fade').bjqs({
+          height      : 450,
+          width       : 886,
+          responsive  : true
+        });
+      });
+      <pre class="secret-source"><code>jQuery(document).ready(function($) {
+$('#banner-fade').bjqs({
+  height      : 450,
+  width       : 886,
+  responsive  : true
+});
+});</code></pre>
+  jQuery(function($) {
+      $('.secret-source').secretSource({
+          includeTag: false
+      });
+  });

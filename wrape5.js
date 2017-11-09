2688d6cfb45c267bfea2695364d64848dc0ae888
danish1u <danishprince786a@gmail.com>
2017-11-09 13:57:19 +0500

    Update and rename wrape4.js to wrape5.js

--- wrape5.js
+++ wrape5.js
@@ -2,26 +2,9 @@ jQuery(document).ready(function(){
 	var $V = jQuery.noConflict();
 
 	$V(".pst-background > tbody > tr > td > table:last-child").addClass("container-fluid addfooter");
-
-
-	if(pageName != "StoresCustomPageHome") {
-		if($V(".addfooter").length > 0) {
-			$V(".addfooter").after(footer);
-		}
-	}
-
-
 	//Getting and Setting Store Categories
-
-
 	//Footer
 	var d = new Date();
 	var footer = '\n\r<div class="footer"><div class="browse-logos"><ul><li><a href="#"><img src="https://raw.githubusercontent.com/danish1u/rick/master/imges/1.png" width="255px;" alt=""></a></li><li><a href="#"><img src="https://raw.githubusercontent.com/danish1u/rick/master/imges/2.png" width="255px;" alt=""></a></li><li><a href="#"><img src="https://raw.githubusercontent.com/danish1u/rick/master/imges/3.png" width="255px;"alt=""></a></li><li><a href="#"><img src="https://raw.githubusercontent.com/danish1u/rick/master/imges/4.png" width="255px;"alt=""></a></li><li><a href="#"><img src="https://raw.githubusercontent.com/danish1u/rick/master/imges/5.png"width="255px;" alt=""></a></li></ul></div><div class="all-rights-reserve">© All rights reserved to Asteria diamonds</div></div>';
-	if(pageName != "PageAboutMeViewStore") {
-		if($V(".addfooter").length > 0) {
-			$V(".addfooter").after(footer);
-		}
-	}
-
 
 });
